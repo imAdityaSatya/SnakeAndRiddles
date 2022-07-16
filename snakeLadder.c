8e477901404332bb9b751605457df9b52bb87e8e
Aditya <73811047+imAdityaSatya@users.noreply.github.com>
2022-07-16 17:52:37 +0530

    Update snakeLadder.c

--- snakeLadder.c
+++ snakeLadder.c
@@ -3,22 +3,24 @@
 #include<time.h>
 
 int chkPos();
-int dice, pos, newPos, tmp, choice, cnt=0;
+int dice, pos, newPos, choice, cnt=0;
 
 int main()
 {
 srand(time(NULL));
-printf("THE SNAKE AND THE LADDER\n\tpress-1: VIEW PLAYBOOK \n\tpress-2: START PLAYING\n");
-printf("You have pressed: ");
+printf("\nTHE SNAKE AND THE LADDER\n");
+printf("~~~~~~~~~~~~~~~~~~~~~~~~");
+printf("\n\tMenu\n\t1. VIEW PLAYBOOK \n\t2. START PLAYING\n");
+printf("Choose an option: ");
 scanf("%d",&choice);
 if(choice==1)
 {
-printf("\nPLAYBOOK:_________________________________________________________________________________________________________\n");
+printf("\nPLAYBOOK:___________________________________________________________________________________________________________\n");
 printf("|                                                                  |                                                |\n");
-printf("|  100    99   98   97   96   95   94   93   92   91               |  1 : START                 99: snake to 41     |\n");
+printf("|  100    99   98   97   96   95   94   93   92   91               |   1: START                 99: snake to 41     |\n");
 printf("|  81     82   83   84   85   86   87   88   89   90               |  89: snake to 53           74: ladder to 92    |\n");
 printf("|  80     79   78   77   76   75   74   73   72   71               |  76: snake to 58           62: ladder to 81    |\n");
-printf("|  61     62   63   64   65   66   67   68   69   70	           |  66: snake to 45           54: snake to 31     |\n");
+printf("|  61     62   63   64   65   66   67   68   69   70               |  66: snake to 45           54: snake to 31     |\n");
 printf("|  60     59   58   57   56   55   54   53   52   51               |  43: snake to 18           50: ladder to 69    |\n");
 printf("|  41     42   43   44   45   46   47   48   49   50               |  40: snake to 3            33: ladder to 90    |\n");
 printf("|  40     39   38   37   36   35   34   33   32   31               |  27: snake to 5            13: ladder to 46    |\n");
@@ -27,16 +29,17 @@ printf("|  20     19   18   17   16   15   14   13   12   11               |    
 printf("|  1       2    3    4    5    6    7    8    9   10               |                 100: THE END                   |\n");
 printf("|__________________________________________________________________|_______________________________________________/\n");
 
-printf("\nReach 100 To Become The Champion \n");
+printf("\nNote:\n -->> Press ENTER to roll the dice\n -->> Reach 100 to WIN the game\n -->> If somehow you happen to reach a position greater than 100\n      then you will be automatically directed to the position-99 \n -->> First dice has already been rolled as a demo\n");
 }
-
+if(choice==2){
+printf("Note:- The first dice has already been rolled for you\n");
+}
 if(choice==1,2)
 {
 printf("\nLET THE GAME BEGIN...\n");
-
 do
 {
-printf("\n\nPress ENTER to roll the dice \n\n");
+printf("\nRoll the dice \n");
 dice-=getchar();
 dice=(rand()%6)+1;
 printf("DICE ROLLED: (%d)\n", dice);
@@ -44,29 +47,30 @@ pos+=dice;
 
 if(dice==6)
 {
-printf("\nWoah its a 6, you get one more chance to roll the dice \n");
+printf("\nWoah its a SIX\nLooks like someone is very lucky with the dice\n");
 printf("Your current pos: [%d]\n", pos);
 continue;
 }
 
 if(pos>100){
-tmp= (pos-100);
-pos= (100-tmp);
-} 
+//tmp= (pos-100);
+//pos= (100-tmp);
+pos=99;
+}
 printf("Your current pos: [%d]\n", pos);
 cnt++;
 
 chkPos();
 
 if(pos<newPos){
-printf("\nWOHOO... you've got on a ladder. \nYour current pos: %d",newPos);
+printf("\nWOHOO... you've got on a ladder. \nYour current pos: [%d]\n",newPos);
 }
 if(pos>newPos){
-printf("\nUH-OH... you're bitten by a snake. \nYour current pos: %d",newPos);
+printf("\nUH-OH... you're bitten by a snake. \nYour current pos: [%d]\n",newPos);
 }
 pos= newPos;
 }while(pos<100);
-printf("CONGRATS!! CHAMPION \nYOU WON in %d throws \n",cnt);
+printf("\nCONGRATS!! CHAMPION \nYOU WON in %d throws \n",cnt);
 }
 getchar();
 return 0;
@@ -82,11 +86,11 @@ case 4:
 
 case 13:
             return newPos=46;
-	        break; 
+            break; 
 
 case 33:
             return newPos=90;
-	        break;
+            break;
 
 case 50:
             return newPos=69;
@@ -110,7 +114,7 @@ case 27:
 
 case 40:
             return newPos=3;
-	        break;
+            break;
 
 case 43:
             return newPos=18;
@@ -126,7 +130,7 @@ case 66:
 
 case 76:
             return newPos=58;
-	        break;
+            break;
 
 case 89:
             return newPos=53;
@@ -141,6 +145,18 @@ default:
 }
 }
 
+int lifeline(){
+if(riddle()==1){
+return 1;
+}else{
+return 0;
+}
+}
+
+
+
+
+/*
 void lifeline(){
 srand(time(NULL));
 int r= rand()%10;
@@ -150,7 +166,6 @@ switch(r)
 case 0: 
     printf("")
 }
-
 }
-
+*/
 
